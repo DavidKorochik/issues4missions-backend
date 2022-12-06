2d3465e255e11a40964f793e6362b5e81ad11955
David Korochik <david1korochik@gmail.com>
2022-12-06 09:30:31 +0200

    Created the users model

---
+++ pkg/models/user.go
@@ -0,0 +1,26 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	UserID         uuid.UUID      `json:"user_id" gorm:"primaryKey"`
+	FirstName      string         `json:"first_name" gorm:"not null"`
+	LastName       string         `json:"last_name" gorm:"not null"`
+	PersonalNumber string         `json:"personal_number" gorm:"not null;unique"`
+	SecretCode     string         `json:"secret_code" gorm:"not null;unique"`
+	PhoneNumber    string         `json:"phone_number" gorm:"not null;unique"`
+	CreatedAt      time.Time      `json:"created_at" gorm:"not null;default:now()"`
+	UpdatedAt      time.Time      `json:"updated_at" gorm:"not null;default:now()"`
+	DeletedAt      gorm.DeletedAt `json:"deleted_at" gorm:"not null"`
+}
+
+func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
+	// We want to use NewRandom, because it returns an error and we want better error handling.
+	u.UserID, err = uuid.NewRandom()
+	return
+}
