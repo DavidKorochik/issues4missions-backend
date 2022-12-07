ecfe4ef5082c5b0b20a3b0fd879f13f3835b8c12
David Korochik <david1korochik@gmail.com>
2022-12-07 07:46:45 +0200

    Deleted the db from return

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,13 +3,10 @@ package main
 import (
 	"github.com/DavidKorochik/issues4missions-backend/pkg/config"
 	"github.com/DavidKorochik/issues4missions-backend/pkg/db"
-	"github.com/DavidKorochik/issues4missions-backend/pkg/server"
 )
 
 func main() {
 	config := config.LoadEnvVariables(".")
-	db := db.ConnectToDB(config)
-	server := server.NewServer(db)
+	db.ConnectToDB(config)
 
-	server.StartServer(config.Port)
 }
