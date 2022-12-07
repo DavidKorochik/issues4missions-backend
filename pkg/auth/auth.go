5f071a294d73d24923b02b5c290958e1cde42f35
David Korochik <david1korochik@gmail.com>
2022-12-07 15:44:47 +0200

    Created an endpoint to authenticate user and generate a jwt

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -3,5 +3,5 @@ package auth
 import "github.com/DavidKorochik/issues4missions-backend/pkg/models"
 
 type Store interface {
-	AuthSture(personalNumber string) (user *models.User, err error)
+	AuthUser(personalNumber string) (user *models.User, err error)
 }
