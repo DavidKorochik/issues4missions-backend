e76477c4cd8aa5a33afe7ccae66bd90632c9205f
David Korochik <david1korochik@gmail.com>
2022-12-07 14:21:54 +0200

    Created the create issue test

--- pkg/handlers/main_test.go
+++ pkg/handlers/main_test.go
@@ -1,11 +1,17 @@
 package handlers
 
 import (
+	"net/http/httptest"
 	"os"
 	"testing"
 
 	"github.com/gin-gonic/gin"
 )
+
+func newTestServer(recorder *httptest.ResponseRecorder) *gin.Engine {
+	_, r := gin.CreateTestContext(recorder)
+	return r
+}
 
 func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
