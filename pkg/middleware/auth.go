5b30df47611bf736e054cb34c86138174f8223db
David Korochik <david1korochik@gmail.com>
2022-12-07 15:58:13 +0200

    Added the auth middleware

--- pkg/middleware/auth.go
+++ pkg/middleware/auth.go
@@ -17,39 +17,41 @@ const (
 	authorizationPayloadKey = "x-auth-token"
 )
 
-func AuthUser(c *gin.Context) {
-	authHeader := c.GetHeader(authorizationHeaderKey)
-	config := config.LoadEnvVariables("../../.")
-	maker := token.NewJWTMaker(config.JWTSecret)
+func AuthUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader(authorizationHeaderKey)
+		config := config.LoadEnvVariables("../../.")
+		maker := token.NewJWTMaker(config.JWTSecret)
 
-	if len(authHeader) == 0 {
-		err := errors.New("Authorization header is not provided")
-		c.AbortWithStatusJSON(http.StatusUnauthorized, util.ErrorResponse(err))
-		return
+		if len(authHeader) == 0 {
+			err := errors.New("Authorization header is not provided")
+			c.AbortWithStatusJSON(http.StatusUnauthorized, util.ErrorResponse(err))
+			return
+		}
+
+		fields := strings.Fields(authHeader)
+
+		if len(fields) < 2 {
+			err := errors.New("Invalid authorization header format")
+			c.AbortWithStatusJSON(http.StatusUnauthorized, util.ErrorResponse(err))
+			return
+		}
+
+		if fields[0] != authorizationHeaderType {
+			err := errors.New("Authorization header type is not provided")
+			c.AbortWithStatusJSON(http.StatusUnauthorized, util.ErrorResponse(err))
+			return
+		}
+
+		payload, err := maker.VerifyJWTToken(fields[1])
+
+		if err != nil {
+			err := errors.New("Authorization token is not valid")
+			c.AbortWithStatusJSON(http.StatusUnauthorized, util.ErrorResponse(err))
+			return
+		}
+
+		c.Set(authorizationPayloadKey, payload)
+		c.Next()
 	}
-
-	fields := strings.Fields(authHeader)
-
-	if len(fields) < 2 {
-		err := errors.New("Invalid authorization header format")
-		c.AbortWithStatusJSON(http.StatusUnauthorized, util.ErrorResponse(err))
-		return
-	}
-
-	if fields[0] != authorizationHeaderType {
-		err := errors.New("Authorization header type is not provided")
-		c.AbortWithStatusJSON(http.StatusUnauthorized, util.ErrorResponse(err))
-		return
-	}
-
-	payload, err := maker.VerifyJWTToken(fields[1])
-
-	if err != nil {
-		err := errors.New("Authorization token is not valid")
-		c.AbortWithStatusJSON(http.StatusUnauthorized, util.ErrorResponse(err))
-		return
-	}
-
-	c.Set(authorizationPayloadKey, payload)
-	c.Next()
 }
