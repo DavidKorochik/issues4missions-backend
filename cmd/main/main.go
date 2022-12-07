b7cefa3c021fd6a34c268795d744392cf8ddd8f6
David Korochik <david1korochik@gmail.com>
2022-12-07 13:25:20 +0200

    Implemented the issues and users store in the main file

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"github.com/DavidKorochik/issues4missions-backend/pkg/config"
 	"github.com/DavidKorochik/issues4missions-backend/pkg/db"
+	"github.com/DavidKorochik/issues4missions-backend/pkg/handlers"
+	"github.com/DavidKorochik/issues4missions-backend/pkg/store"
 )
 
 func main() {
 	config := config.LoadEnvVariables(".")
-	db.ConnectToDB(config)
 
+	db := db.ConnectToDB(config)
+
+	is := store.NewIssueStore(db)
+	us := store.NewUserStore(db)
+
+	handlers.NewHandler(us, is)
 }
